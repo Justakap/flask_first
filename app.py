7bf5dce5bab772510474c56744972fa7962615f5
Justakap <119529108+Justakap@users.noreply.github.com>
2024-06-07 21:08:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from prettytable import PrettyTable
 import re
 from flask_cors import CORS
 from PyPDF2 import PdfReader
@@ -12,7 +11,7 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/send', methods=['POST'])
+@app.route("/send", methods=["POST"])
 def receive_data():
     data = request.json  # Extract JSON data from the request body
     # Process the received data as needed
@@ -44,57 +43,76 @@ def receive_data():
         "D": 5.5,
         "E+": 5,
         "E": 4.5,
-        "F": 0
+        "F": 0,
     }
 
     roll_no_pattern = r"Roll No\s*:\s*(\w+)"
     roll_no_match = re.search(roll_no_pattern, text)
-    roll_no = roll_no_match.group(1) if roll_no_match else ''
+    roll_no = roll_no_match.group(1) if roll_no_match else ""
 
     remark_pattern = r"REMARKS\s*:\s*(\w+)"
     remark_match = re.search(remark_pattern, text)
-    remark = remark_match.group(1) if remark_match else ''
+    remark = remark_match.group(1) if remark_match else ""
 
     name_pattern = r"Name\s*:\s*(.*)\s*Father's Name"
     name_match = re.search(name_pattern, text)
-    name = name_match.group(1).strip() if name_match else ''
+    name = name_match.group(1).strip() if name_match else ""
 
     sem_name_pattern = r"B. Tech\s*(.*)\s*"
     sem_name_match = re.search(sem_name_pattern, text)
-    sem_name = sem_name_match.group(1).strip() if sem_name_match else ''
+    sem_name = sem_name_match.group(1).strip() if sem_name_match else ""
 
     father_name_pattern = r"Father's Name\s*:\s*(.*)\s*"
     father_name_pattern = re.search(father_name_pattern, text)
-    father_name = father_name_pattern.group(
-        1).strip() if father_name_pattern else ''
-    
+    father_name = father_name_pattern.group(1).strip() if father_name_pattern else ""
+
     sem_no_pattern = r"(\w+-\d+)"
     sem_no_pattern = re.search(sem_no_pattern, text)
-    sem_no = sem_no_pattern.group(
-        1).strip() if sem_no_pattern else ''
+    sem_no = sem_no_pattern.group(1).strip() if sem_no_pattern else ""
 
     College_name_pattern = r"College Name\s*:\s*(.*)\s*"
     College_name_pattern = re.search(College_name_pattern, text)
-    College_name = College_name_pattern.group(
-        1).strip() if College_name_pattern else ''
+    College_name = College_name_pattern.group(1).strip() if College_name_pattern else ""
 
     Sodeca_name_pattern = r"SODECA (\w+-\d+) (\d+) (\w\++)"
     matches = re.findall(Sodeca_name_pattern, text)
-    sodeca_data = [{'Sodeca_Code': match1[0], 'Sodeca_EndTerm': match1[1],
-                    "Grade:": match1[2]} for match1 in matches]
+    sodeca_data = [
+        {"Sodeca_Code": match1[0], "Sodeca_EndTerm": match1[1], "Grade:": match1[2]}
+        for match1 in matches
+    ]
 
-    pattern = r"(.+?) (\w+-\d+) (\d+) (\d+) (\w+\+*)"
+    # pattern = r"(.+?) (\w+-\d+) (\d+) (\d+) (\w+\+*)"
 
-    matches = re.findall(pattern, text)
+    # matches = re.findall(pattern, text)
 
-    table_data = [{'Subject_Name': match[0], 'Subject_Code': match[1],
-                   'Internal_Marks': match[2], 'External_Marks': match[3],
-                   'Total_Marks': str(int(match[2]) + int(match[3])), 'Grade': match[4], 'Final_Marks': int(match[2]) + int(match[3]), 'Points': gradeMapping.get(match[4], 0)} for match in matches]
+    patterns = [
+        r"(.+?) (\w+-\d+) (\d+) (\d+) (\w+\+*)",  # Pattern to matches like 8CS4-01
+        r"(.+?) (\w+-\d+\.\d+) (\d+) (\d+) (\w+\+*)",  # Pattern to match like   8AG6-60.1
+    ]
+
+    # Find all matches in the text
+    matches = []
+    for pattern in patterns:
+        matches.extend(re.findall(pattern, text))
+
+    table_data = [
+        {
+            "Subject_Name": match[0],
+            "Subject_Code": match[1],
+            "Internal_Marks": match[2],
+            "External_Marks": match[3],
+            "Total_Marks": str(int(match[2]) + int(match[3])),
+            "Grade": match[4],
+            "Final_Marks": int(match[2]) + int(match[3]),
+            "Points": gradeMapping.get(match[4], 0),
+        }
+        for match in matches
+    ]
 
     # table_data = sorted(table_data, key=lambda x: x['Total_Marks'], reverse=True)
 
-    final_marks = sum(row['Final_Marks'] for row in table_data)
-    total_points = sum(row['Points'] for row in table_data)
+    final_marks = sum(row["Final_Marks"] for row in table_data)
+    total_points = sum(row["Points"] for row in table_data)
 
     total_rows = len(table_data)
     data = {
@@ -108,14 +126,15 @@ def receive_data():
         "Subjects": table_data,
         "Sodeca": sodeca_data,
         "total_points": total_points,
-        "semester":sem_no[0:1]
+        "semester": sem_no[0:1],
     }
     json_data = json.dumps(data, indent=4)
 
     # Send back a response
     # response_data = {"message": "Data received successfully"}
+
     return json_data, 200
 
 
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == "__main__":
+    app.run()
